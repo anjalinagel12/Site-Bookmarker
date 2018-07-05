1989cc02131e558bc921faa5530d953c00e74351
anjalinagel12 <anjali12.ins@gmail.com>
2018-07-05 23:39:19 +0530

    "new commit"

--- bookmarker/js/main.js
+++ bookmarker/js/main.js
@@ -3,12 +3,57 @@
 
 document.getElementById('myForm').addEventListener('submit',saveBookmark);
 
+
+
+
+function deleteBookmark(url)
+{
+    var bookmarks= JSON.parse(localStorage.getItem('bookmarks'));
+    for(var i=0;i<bookmarks.length;i++)
+    {
+        if(bookmarks[i].url==url)
+        {
+            bookmarks.splice(i,1); 
+        }
+    }
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks();
+  
+     
+}
+
+(function fetchBookmarks()
+{
+     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+     //console.log(bookmarks);
+     var bookmarksResults= document.getElementById('bookmarksResults');
+     bookmarksResults.innerHTML =  '';
+     for(var i=0;i<bookmarks.length;i++)
+     {
+         var name = bookmarks[i].name;
+         var url = bookmarks[i].url;
+         bookmarksResults.innerHTML += '<div class="well">'+
+                                        '<h3>'+name+
+                                        '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> '+
+                                        '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger"  href="#">Delete</a> '+
+
+                                        '</h3>'+
+                                        '</div>' ;
+          
+     }
+})();
+
 //save bookmark 
 function saveBookmark(e)
 {
-    var siteName = document.getElementById('sitename').value ;
-    var siteUrl = document.getElementById('siteURL').value;
+    var siteName = document.getElementById("sitename").value ;
+    var siteUrl = document.getElementById("siteURL").value;
     
+    if(!siteName || !siteUrl){
+        alert("please fill the form");
+        return False; 
+    }
+
     var bookmark = {
         name: siteName,
         url: siteUrl
@@ -28,21 +73,15 @@ function saveBookmark(e)
         //set to local storage 
         localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
     }
-   /* else
+    else
     {
         var bookmarks= JSON.parse(localStorage.getItem('bookmarks'));
         bookmarks.push(bookmark);
         localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
-    }*/
+    }
+    fetchBookmarks();
     //get form values 
      e.preventDefault();
        
 }
 
-function fetchBookmarks()
-{
-     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-     //console.log(bookmarks);
-     var bookmarksResukt= document.getElementById('bookmarksResults');
-     bookmarksResults.innerHTML="HELLO";
-}
